99a2e5a1410d2494ccc10eacb07c458194944380
DanielBarretoAlves <danielbarreto559@gmail.com>
2022-10-18 16:42:01 -0300

    Generate Information

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import json
 from flask import Flask, render_template, request, redirect
-from flask import Flask
+from flask import Flask, jsonify, json
+
 
 def index():
     pronto = {}
@@ -8,22 +10,29 @@ def index():
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+
+@app.route("/dada", methods=["GET"])
 def home():
     return render_template('index.html')
 
-@app.route("/donos", methods=["GET"])
+
+@app.route("/test", methods=["POST"])
 def dono():
-    return render_template('dono.html')
+    valor = request.form.get('valor')
+    print(valor)
+    return valor
+    
 
-
-@app.route('/dono')
+@app.route('/')
 def show_form():
+    # Console.log("teste1")
+    print("teste")
     return render_template('dono.html')
 
 
-
-@app.route("/result", methods=['POST'])
-def show_result():
-    result = request.form
-    return render_template('dono.html', result=result)
+# @app.route("/result", methods=['POST'])
+# def show_result():
+#     print("here")
+#     result = request.form
+#     # console.log(result)
+#     return render_template('dono.html', result=result)
